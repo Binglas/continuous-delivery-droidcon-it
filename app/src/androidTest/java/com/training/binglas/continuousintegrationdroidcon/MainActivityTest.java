321fdb9cb2289c967639ee8a6852ef904d8d447a
jzao <joao.zao@blip.pt>
2017-04-06 15:10:34 +0200

    Add test

---
+++ app/src/androidTest/java/com/training/binglas/continuousintegrationdroidcon/MainActivityTest.java
@@ -0,0 +1,26 @@
+package com.training.binglas.continuousintegrationdroidcon;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+import android.support.test.rule.ActivityTestRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * Created by joaozao on 06/04/17.
+ */
+
+public class MainActivityTest {
+
+  @Rule
+  public ActivityTestRule<MainActivity> rule = new
+      ActivityTestRule < MainActivity > (MainActivity.class);
+
+  @Test
+  public void showHelloWorld() {
+    onView(withText("Hello world!")).check(matches(withText(R.string.hello)));
+
+  }
+}
